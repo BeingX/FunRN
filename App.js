ac7937714d20b87555c5c0965abc040de9733b3e
Qixing Yin <qixingyin@gmail.com>
2019-02-01 23:37:35 +0800

    gain data and show as list

--- App.js
+++ App.js
@@ -12,12 +12,13 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import SearchPage from './SearchPage';
-
+import SearchResults from './SearchResults';
 
 const AppNavigator = createStackNavigator({
   Home: {
-    screen: SearchPage
-  }
+    screen: SearchPage,
+  },
+  Results: { screen: SearchResults },
 });
 
 const AppContainer = createAppContainer(AppNavigator);
